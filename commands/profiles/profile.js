c460b7aaeebf596be93e4cd2ffec7f98c0ffa731
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-01-26 14:07:20 -0700

    Update profile.js

--- commands/profiles/profile.js
+++ commands/profiles/profile.js
@@ -1,1 +1,22 @@
+const { SlashCommandBuilder } = require("discord.js");
+const fs = require("fs");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("profile")
+    .setDescription("View your KartKings profile"),
+
+  async execute(interaction) {
+    const players = JSON.parse(fs.readFileSync("./data/players.json"));
+    const player = players.find(p => p.discord_id === interaction.user.id);
+
+    if (!player) {
+      return interaction.reply({ content: "You are not registered. Use /register", ephemeral: true });
+    }
+
+    await interaction.reply(
+      `🏁 **${player.kart_name}**\nWins: ${player.wins}\nLosses: ${player.losses}\nKOs: ${player.kos}\nRank: ${player.rank}`
+    );
+  }
+};
 

00e242fb3451116c13e73202527c287942a2ae92
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-01-27 23:01:23 -0700

    Update support.js

--- commands/support/support.js
+++ commands/support/support.js
@@ -1,1 +1,15 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('support')
+    .setDescription('Get help from the KartKings team'),
+
+  async execute(interaction) {
+    await interaction.reply({
+      content: "📞 **Support Info:**\n- Contact a moderator on Discord for urgent issues.\n- Use `/help` for a full list of commands.\n- Check `/faq` for common questions.\n\nWe’re here to help you dominate the arena!",
+      ephemeral: true
+    });
+  }
+};
 

76fd1757cd86b7f1a321c024636cff47ed32f8fb
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-01-27 19:50:08 -0700

    Update signup.js

--- commands/tournaments/signup.js
+++ commands/tournaments/signup.js
@@ -1,1 +1,24 @@
+// commands/tournaments/signup.js
+const { SlashCommandBuilder } = require('discord.js');
+const tournamentService = require('../../services/tournamentService');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('signup')
+    .setDescription('Join a tournament')
+    .addStringOption(option =>
+      option.setName('tournament')
+        .setDescription('Tournament ID')
+        .setRequired(true)),
+  async execute(interaction) {
+    const tournamentId = interaction.options.getString('tournament');
+    const tournament = tournamentService.addParticipant(tournamentId, interaction.user.id);
+
+    if (!tournament) {
+      return interaction.reply({ content: '❌ Tournament not found!', ephemeral: true });
+    }
+
+    await interaction.reply(`✅ You have joined the tournament: ${tournament.name}`);
+  },
+};
 

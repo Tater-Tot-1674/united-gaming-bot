569dc5e3aedb0245ce9b32e42d019a8fcb176348
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-01-27 19:47:39 -0700

    Update playerService.js

--- services/playerService.js
+++ services/playerService.js
@@ -1,1 +1,41 @@
+// services/playerService.js
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, '../data/players.json');
+
+function loadPlayers() {
+  if (!fs.existsSync(filePath)) return {};
+  const raw = fs.readFileSync(filePath);
+  return JSON.parse(raw);
+}
+
+function savePlayers(players) {
+  fs.writeFileSync(filePath, JSON.stringify(players, null, 2));
+}
+
+function getPlayer(id) {
+  const players = loadPlayers();
+  return players[id] || null;
+}
+
+function addPlayer(id, data) {
+  const players = loadPlayers();
+  players[id] = data;
+  savePlayers(players);
+}
+
+function updatePlayer(id, data) {
+  const players = loadPlayers();
+  players[id] = { ...(players[id] || {}), ...data };
+  savePlayers(players);
+}
+
+module.exports = {
+  loadPlayers,
+  savePlayers,
+  getPlayer,
+  addPlayer,
+  updatePlayer,
+};
 

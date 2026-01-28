7a5a8054b1c057a2e4fe17e4b1a1d2ea9ca1bbc9
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-01-27 23:08:18 -0700

    Update syncToSync.js

--- utils/syncToSync.js
+++ utils/syncToSync.js
@@ -1,1 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+
+const siteDataPath = path.join(process.cwd(), '../kartkings-site/data');
+
+export async function syncToSite(fileName, data) {
+  try {
+    const filePath = path.join(siteDataPath, fileName);
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
+    console.log(`Synced ${fileName} to site data.`);
+    return true;
+  } catch (error) {
+    console.error(`Error writing ${fileName} to site:`, error);
+    return false;
+  }
+}
 

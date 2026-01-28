f032e3d212ae8919066bebbd6ba1dc9e59c1c3c6
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-01-27 22:50:14 -0700

    Update leaderboardCalc.js

--- utils/leaderboardCalc.js
+++ utils/leaderboardCalc.js
@@ -1,40 +1,20 @@
 const fs = require("fs");
 const path = require("path");
-const { rankSystem } = require("./rankSystem");
 
-const playersPath = path.join(__dirname, "../data/players.json");
-const weeklyPath = path.join(__dirname, "../data/leaderboard_weekly.json");
-const monthlyPath = path.join(__dirname, "../data/leaderboard_monthly.json");
+const leaderboardPath = path.join(__dirname, "../data/leaderboard_weekly.json");
 
-function calculateLeaderboards() {
-  const players = JSON.parse(fs.readFileSync(playersPath, "utf8"));
+function addWin(playerId) {
+  let leaderboard = JSON.parse(fs.readFileSync(leaderboardPath, "utf8"));
 
-  const playerArray = Object.values(players);
+  if (!leaderboard[playerId]) {
+    leaderboard[playerId] = { wins: 0 };
+  }
 
-  const formatted = playerArray.map(player => ({
-    id: player.id,
-    discord: player.discord,
-    name: player.name || "Unknown",
-    xp: player.xp || 0,
-    wins: player.wins || 0,
-    losses: player.losses || 0,
-    rank: rankSystem.getRank(player.xp)
-  }));
+  leaderboard[playerId].wins += 1;
 
-  // 🥇 Sort by XP (main ranking)
-  formatted.sort((a, b) => b.xp - a.xp);
-
-  const leaderboard = formatted.map((player, index) => ({
-    position: index + 1,
-    ...player
-  }));
-
-  // Save both (you can separate logic later)
-  fs.writeFileSync(weeklyPath, JSON.stringify(leaderboard, null, 2));
-  fs.writeFileSync(monthlyPath, JSON.stringify(leaderboard, null, 2));
-
-  return leaderboard;
+  fs.writeFileSync(leaderboardPath, JSON.stringify(leaderboard, null, 2));
 }
 
-module.exports = { calculateLeaderboards };
+module.exports = { addWin };
+
 

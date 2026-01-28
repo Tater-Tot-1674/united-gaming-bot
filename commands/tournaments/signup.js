571daa57c1338e7b97739210c00ca33a946eb1d2
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-01-28 00:10:57 -0700

    Update signup.js

--- commands/tournaments/signup.js
+++ commands/tournaments/signup.js
@@ -1,3 +1,9 @@
+const DISCORD_TOKEN = process.env.DISCORDTOKEN;
+const GITHUB_TOKEN = process.env.GITHUBTOKEN;
+const WEBSITE_REPO = process.env.GITHUBREPO;
+const BOT_USER_ID = process.env.BOTUSERID;
+const RENDER_URL = process.env.RENDERSERVICEURL; // optional
+
 const fs = require('fs');
 const path = require('path');
 const { syncToSite } = require('../../utils/syncToSite');
@@ -28,6 +34,5 @@ module.exports = {
     return interaction.reply({ content: `You have joined ${tournament.name}!`, ephemeral: true });
   }
 };
-
 
 

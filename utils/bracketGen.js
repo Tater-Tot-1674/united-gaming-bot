3661973cb63f89114214ece8559e64bc72b40781
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-01-28 11:06:08 -0700

    Update bracketGen.js

--- utils/bracketGen.js
+++ utils/bracketGen.js
@@ -1,32 +1,24 @@
-module.exports = function generateBracket(players) {
-  players = players.sort(() => Math.random() - 0.5);
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
 
-  const rounds = [];
+function generateBracket(players) {
+  const shuffled = shuffle([...players]);
+  const bracket = [];
 
-  // ROUND 1
-  let round = [];
-  for (let i = 0; i < players.length; i += 2) {
-    round.push({
-      p1: players[i],
-      p2: players[i + 1] || null,
+  for (let i = 0; i < shuffled.length; i += 2) {
+    bracket.push({
+      matchId: `match_${i / 2 + 1}`,
+      player1: shuffled[i],
+      player2: shuffled[i + 1] || null,
       winner: null
     });
   }
 
-  rounds.push(round);
-
-  // Future empty rounds placeholder
-  let size = round.length;
-  while (size > 1) {
-    size = Math.ceil(size / 2);
-    rounds.push(Array(size).fill({ p1: null, p2: null, winner: null }));
-  }
-
   return {
     created: new Date().toISOString(),
-    currentRound: 0,
-    rounds
+    rounds: [bracket]
   };
-};
+}
 
-
+module.exports = { generateBracket };

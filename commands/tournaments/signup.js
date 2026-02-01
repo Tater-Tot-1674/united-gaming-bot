90469ef484ecfecb9313c6ef3b9a30019b877d12
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-02-01 13:11:04 -0700

    Update signup.js

--- commands/tournaments/signup.js
+++ commands/tournaments/signup.js
@@ -1,14 +1,9 @@
-const DISCORD_TOKEN = process.env.DISCORDTOKEN;
-const GITHUB_TOKEN = process.env.GITHUBTOKEN;
-const WEBSITE_REPO = process.env.GITHUBREPO;
-const BOT_USER_ID = process.env.BOTUSERID;
-const RENDER_URL = process.env.RENDERSERVICEURL; // optional
-
 const fs = require('fs');
 const path = require('path');
+const { DATA_PATHS } = require('../../utils/constants');
 const { syncToSite } = require('../../utils/syncToSite');
 
-const tournamentsPath = path.join(__dirname, '../../data/tournaments.json');
+const tournamentsPath = path.join(__dirname, '../../', DATA_PATHS.TOURNAMENTS);
 
 module.exports = {
   name: 'signup',
@@ -17,22 +12,22 @@ module.exports = {
     const tournamentId = interaction.options.getString('tournament_id');
     const userId = interaction.user.id;
 
-    const tournaments = JSON.parse(fs.readFileSync(tournamentsPath));
+    const tournaments = JSON.parse(fs.readFileSync(tournamentsPath, 'utf8'));
     const tournament = tournaments.find(t => t.id === tournamentId);
     if (!tournament) return interaction.reply({ content: 'Tournament not found', ephemeral: true });
 
-    if (tournament.participants?.includes(userId)) {
+    tournament.participants = tournament.participants || [];
+    if (tournament.participants.includes(userId)) {
       return interaction.reply({ content: 'You are already signed up!', ephemeral: true });
     }
 
-    tournament.participants = tournament.participants || [];
     tournament.participants.push(userId);
-
     fs.writeFileSync(tournamentsPath, JSON.stringify(tournaments, null, 2));
-    syncToSite('tournaments.json'); // 🔥 live update
+    syncToSite('tournaments.json');
 
     return interaction.reply({ content: `You have joined ${tournament.name}!`, ephemeral: true });
   }
 };
+
 
 

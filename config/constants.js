87d3585e913eea481b4ef0690ca18fa985251009
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-02-01 13:01:47 -0700

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,8 @@
 // kartkings-bot/config/constants.js
 
-export const DISCORD_TOKEN = process.env.DISCORDTOKEN;
-export const GITHUB_TOKEN = process.env.GITHUBTOKEN;
-export const WEBSITE_REPO = process.env.GITHUBREPO;
-export const BOT_USER_ID = process.env.BOTUSERID;
-export const RENDER_URL = process.env.RENDERSERVICEURL; // optional
+export const DISCORTOKEN = process.env.DISCORDTOKEN;
+export const GITHUBTOKEN = process.env.GITHUBTOKEN;
+export const WEBSITEREPO = process.env.GITHUBREPO;
+export const BOTUSERID = process.env.BOTUSERID;
+export const RENDERSERVICEURL = process.env.RENDERSERVICEURL; // optional
 

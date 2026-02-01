5f8f145affd9edd6089a4a260a9644d242488f3e
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-02-01 13:11:23 -0700

    Update bracket.js

--- commands/tournaments/bracket.js
+++ commands/tournaments/bracket.js
@@ -1,16 +1,11 @@
-const DISCORD_TOKEN = process.env.DISCORDTOKEN;
-const GITHUB_TOKEN = process.env.GITHUBTOKEN;
-const WEBSITE_REPO = process.env.GITHUBREPO;
-const BOT_USER_ID = process.env.BOTUSERID;
-const RENDER_URL = process.env.RENDERSERVICEURL; // optional
-
 const fs = require('fs');
 const path = require('path');
+const { DATA_PATHS } = require('../../utils/constants');
 const { generateBracket } = require('../../utils/bracketGen');
 const { syncToSite } = require('../../utils/syncToSite');
 
-const tournamentsPath = path.join(__dirname, '../../data/tournaments.json');
-const bracketPath = path.join(__dirname, '../../data/bracket.json');
+const tournamentsPath = path.join(__dirname, '../../', DATA_PATHS.TOURNAMENTS);
+const bracketPath = path.join(__dirname, '../../', DATA_PATHS.BRACKET);
 
 module.exports = {
   name: 'bracket',
@@ -18,13 +13,13 @@ module.exports = {
   async execute(interaction) {
     const tournamentId = interaction.options.getString('tournament_id');
 
-    const tournaments = JSON.parse(fs.readFileSync(tournamentsPath));
+    const tournaments = JSON.parse(fs.readFileSync(tournamentsPath, 'utf8'));
     const tournament = tournaments.find(t => t.id === tournamentId);
     if (!tournament) return interaction.reply({ content: 'Tournament not found', ephemeral: true });
 
     const bracket = generateBracket(tournament.participants || []);
     fs.writeFileSync(bracketPath, JSON.stringify(bracket, null, 2));
-    syncToSite('bracket.json'); // 🔥 live update
+    syncToSite('bracket.json');
 
     return interaction.reply({ content: `Bracket for ${tournament.name} generated!`, ephemeral: true });
   }

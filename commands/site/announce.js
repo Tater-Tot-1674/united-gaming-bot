a1bd4f5a67a3c4b870ccabfaadddcb232ea684a4
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-02-02 20:07:46 -0700

    Update announce.js

--- commands/site/announce.js
+++ commands/site/announce.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { DATA_PATHS } = require('../../utils/constants');
+const { DATA_PATHS, WEBSITE_REPO, GITHUB_TOKEN } = require('../../utils/constants');
 const { syncToSite } = require('../../utils/syncToSite');
 
 const announcementsPath = path.join(__dirname, '../../', DATA_PATHS.ANNOUNCEMENTS);
@@ -8,22 +8,36 @@ const announcementsPath = path.join(__dirname, '../../', DATA_PATHS.ANNOUNCEMENT
 module.exports = {
   name: 'announce',
   description: 'Post an announcement to the site',
+
   async execute(interaction) {
     const content = interaction.options.getString('message');
     const user = interaction.user.username;
 
-    const announcements = JSON.parse(fs.readFileSync(announcementsPath, 'utf8'));
+    let announcements;
+    try {
+      announcements = JSON.parse(fs.readFileSync(announcementsPath, 'utf8'));
+    } catch (err) {
+      console.error('❌ Failed to read announcements.json:', err);
+      return interaction.reply({ content: 'Error reading announcements.', ephemeral: true });
+    }
+
     const newAnnouncement = {
       id: Date.now().toString(),
       author: user,
       content,
       date: new Date().toISOString()
     };
+
     announcements.push(newAnnouncement);
 
-    fs.writeFileSync(announcementsPath, JSON.stringify(announcements, null, 2));
-    syncToSite('announcements.json');
+    try {
+      fs.writeFileSync(announcementsPath, JSON.stringify(announcements, null, 2));
+      syncToSite('announcements.json', WEBSITE_REPO, GITHUB_TOKEN);
+    } catch (err) {
+      console.error('❌ Failed to write announcements.json:', err);
+    }
 
     return interaction.reply({ content: 'Announcement posted!', ephemeral: true });
   }
 };
+

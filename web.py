4e705e4979629b2ff9fab59b644b672aef5c3165
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-02-03 22:50:01 -0700

    Update web.py

--- web.py
+++ web.py
@@ -1,3 +1,6 @@
+import os
+print(">>> WORKING DIRECTORY:", os.getcwd())
+print(">>> FILES HERE:", os.listdir("."))
 print(">>> IMPORTED FILE:", __file__)
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
